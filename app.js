9d4ef4f4fc18d5a9a4e533b3e95bf2630f56a0cf
sofiaanahi <sofiaanahirodas@gmail.com>
2023-07-03 01:14:56 -0300

    funciona la bd feli

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 
-const path = require('path');
+require("dotenv").config();
+require("ejs");
+
+//se conecta la base de datos
+// Se importa la instancia de conexión a la base de datos - (debe ser después de leer las variables de entorno)
+const { sequelize } = require("./database");
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Conexión a base de datos exitosa"))
+  .catch((error) => console.log("Error al conectar a base de datos", error));
+
+//inicializamos el puerto
 
 const app = express();
+const port = process.env.PORT || 7000;
 
 // Middlewares
 // TODO: Implementar middlewares
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+app.use((req, res, next) => {
+  return res.status(404).render("404");
+});
+
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () =>
+  console.log(`el servidor funciona putamadre http://localhost:${port}`)
+);
--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,73 @@
 // TODO: Crear modelo de datos de Reserva
+
+const { DataTypes, sequelize } = require("./database");
+
+const reserva = sequelize.define(
+  "reserva",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    codigo: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      defaultValue: new Date().getTime(),
+    },
+    reserva: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    fechaReserva: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    habitacion: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    cantidadPersona: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    createAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defualtValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    deleteAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    createAt: true,
+    updateAt: true,
+    deletedAt: true,
+    tableName: "reservas",
+  }
+);
+reserva.sync({ force: false }).then(() => {
+  console.log("tabla de reservas creada");
+});
+
+module.exports = reserva;
